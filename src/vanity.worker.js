6b6e929a5397743916a2e219d593da5c51005952
Grzegorz Kućmierz <gkucmierz@gmail.com>
2022-09-24 16:22:44 +0200

    finish basic version

--- src/vanity.worker.js
+++ src/vanity.worker.js
@@ -1,55 +1,85 @@
 
 import pk2a from 'ethereum-private-key-to-address';
-// console.log(pk2a('4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d'))
 
-const SEND_TIMEOUT = 100;
+const SEND_TIMEOUT = 200;
 
 const randomAddress = (buffer = Buffer(32)) => {
   self.crypto.getRandomValues(buffer);
   return pk2a(buffer);
 };
 
-const sendWork = data => {
-  postMessage(data);
-};
-
 const clipAddr = addr => addr.substr(2);
 
-const runner = (matchFn) => {
+const buff2str = buffer => {
+  return `0x${[...buffer].map(n => n.toString(16).padStart(2, '0')).join('')}`;
+};
+
+const runner = (sendWork => {
   const buffer = Buffer(32);
   let matches = [];
-  let date = +new Date();
   let cnt = 0;
-  const forever = true;
-  while (forever) {
-    const now = +new Date();
-    if (now > date) {
-      date = now + SEND_TIMEOUT;
-      sendWork({ cnt, matches });
-      matches = [];
+  let matcher;
+  let stop = true;
+
+  const loop = date => {
+    let now;
+    do {
+      const address = randomAddress(buffer);
+      if (matcher(clipAddr(address))) {
+        matches.push([address, buff2str(buffer)]);
+      }
+      ++cnt;
+      now = +new Date();
+    } while(now < date);
+    sendWork({ cnt, matches });
+    matches = [];
+    cnt = 0;
+    if (!stop) {
+      setTimeout(() => loop(+new Date() + SEND_TIMEOUT));
     }
-    const address = randomAddress(buffer);
-    ++cnt;
-    if (matchFn(clipAddr(address))) {
-      matches.push(address);
-    }
+  };
+
+  return {
+    start: () => {
+      console.log('start')
+      const lastStop = stop;
+      stop = false;
+      if (lastStop) {
+        loop(+new Date());
+      }
+    },
+    stop: () => {
+      stop = true;
+    },
+    setMatcher: m => matcher = m
+  };
+})(data => postMessage(data));
+
+const matchers = {
+  zeros: (len = 3) => {
+    const match = '0'.repeat(len);
+    return addr => addr.substr(0, len) === match;
+  },
+  leading: (match = '000') => {
+    const len = match.length;
+    return addr => addr.substr(0, len) === match;
+  },
+  regex: (regex = /^000/) => addr => regex.test(addr)
+};
+
+self.addEventListener('message', e => {
+  const data = e.data;
+  switch (data.action) {
+    case 'updateRegex':
+      runner.setMatcher(matchers.regex(data.regex));
+      break;
+    case 'start':
+      runner.start();
+      break;
+    case 'stop':
+      runner.stop();
+      break;
   }
-};
-
-const zeros = (len = 3) => {
-  const match = '0'.repeat(len);
-  return addr => addr.substr(0, len) === match;
-};
-
-// const leading = (match = '000') => {
-//   const len = match.length;
-//   return addr => addr.substr(0, len) === match;
-// };
-
-self.addEventListener('message', () => {
-  // console.log('worker got, ', e.data, '12312312');
-
-  runner(zeros(3));
-
-  // postMessage({a:'123'});
 });
+
+runner.setMatcher(matchers.zeros());
